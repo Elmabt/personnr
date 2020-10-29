2ccdf708bdfaaa9460ee80dd84668725419c1005
elma bt <elmabristric20@gmail.com>
2020-10-29 14:46:28 +0100

    Add project files.

---
+++ personnr/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace personnr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in ett 12 sifrigt personnummer: ");
+            var personnummer = Console.ReadLine();
+            
+        }
+    }
+}
