b2207e6009e35bb9b80afc7fc091c9a0a740c105
elma bt <elmabristric20@gmail.com>
2020-11-02 00:32:28 +0100

    min sitta uppdatering har varit dålg med att uppdatera. det har jag långtasamt med säckert jobbat med i två dar men inte syncat det med github vilket var dumt av mig

--- personnr/Program.cs
+++ personnr/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace personnr
 {
@@ -9,7 +10,6 @@ namespace personnr
             //programmet kommer fråga om ett 12 sifftrigt personnummer
             Console.WriteLine("skriv in ett 12 sifrigt personnummer: ");
             var personnummer = Console.ReadLine();
-
 
             //denna funktion ska kontrollera att det är ett 12 siffrigt nummer och inte 11 eller 13 siffrigt nummer
             // || = Resultatet av x || y är sant om antingen x eller y utvärderas till sant. Annars är resultatet falskt
@@ -22,7 +22,7 @@ namespace personnr
             }
             //denna funktioon är för att se om datumet är korekt alltså de 8 första siffrorna 
             var date = personnummer.Substring(0, 8);
-            var datumanalys = DateTime.TryParse(date.Insert(4, "-").Insert(7, "-"), out var datumcon);
+            var datumanalys = DateTime.TryParse(date.Insert(7,"-").Insert(4,"-"), out var datumcon);
             //om datumet är fel
             if(!datumanalys)
             {
@@ -31,7 +31,35 @@ namespace personnr
                 Console.ReadLine();
                 return;
             }
+            //funktionen för att se om året är mellan 1753 och 2020 
+             if (datumcon.Year < 1753 || datumcon.Year > 2020)
+
+            {
+                Console.WriteLine("året måsdte vara i mellan 1753-2020");
+                var Year = Console.ReadLine();
+                return;
+            }
+            // denna funktion kommer vara för att de om det är en kvinna eller man.
+            String siffras = personnummer.Substring(8, 1);
+            int siffrascon = int.Parse(siffras);
+            int result = (siffrascon % 2); 
+            //om det ären udda siffra så är det en man 
+            if(result == 1)
+            {
+                Console.WriteLine("det är en man");
+            }
+            //om det inte är ett udda siffra så är det en 
+            else
+            {
+                Console.WriteLine("det är en kvinna");
+            }
+            //när programmetär genomfört så ska detta medelande upp
+            Console.WriteLine(" personummret är rätt tryck på en random knapp för att avsluta : D ");
+
             
-        }
+
+
+
+                }
     }
 }
